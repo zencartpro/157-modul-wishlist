d309cdc308219c7be386bc2ec928620725d33c5e
Jörg Schönberger <office@webchills.at>
2024-03-16 09:25:08 +0100

    prevent PHP 8.2 warning logs

--- NEUE DATEIEN/includes/extra_cart_actions/wishlist_cart_actions.php
+++ NEUE DATEIEN/includes/extra_cart_actions/wishlist_cart_actions.php
@@ -6,7 +6,7 @@
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: wishlist_cart_actions.php 2024-03-16 08:02:16Z webchills $
+ * @version $Id: wishlist_cart_actions.php 2024-03-16 09:19:16Z webchills $
  */
 if (!defined('IS_ADMIN_FLAG')) {
   die('Illegal Access');
@@ -21,17 +21,20 @@ switch ($_GET['action']) {
 			if ( $_SESSION['cart']->get_quantity($_GET['products_id']) == 1 ){
 			$_SESSION['cart']->remove($_GET['products_id']);
 			} else {
-				$quantnow = $_SESSION['cart']->get_quantity($_GET['products_id']);				
+				$quantnow = $_SESSION['cart']->get_quantity($_GET['products_id']);	
+				if (isset($_POST['id'])) {			
 				$_SESSION['cart']->update_quantity($_GET['products_id'], $quantnow, $_POST['id']);
+			}
 			}
 				if (!zen_is_logged_in()) {
     				$_SESSION['navigation']->set_snapshot();
     				zen_redirect(zen_href_link(FILENAME_LOGIN, '', 'SSL'));
 					}
 				if ($_SESSION['customer_id'] && isset($_GET['products_id'])) {
-				
+					$attributes = '';
+				if (isset($_POST['id']) && is_array($_POST['id'])) {
 				(is_array($_POST['id']) ? $attributes = serialize( $_POST['id'] ) : $attributes = '');				
-				
+			}
 				require_once(DIR_WS_CLASSES . 'un_wishlist.php');
 				$oWishlist = new un_wishlist($_SESSION['customer_id']);
 				$oWishlist->addProduct((int)$_GET['products_id'], $attributes);
